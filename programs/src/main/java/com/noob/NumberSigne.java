8af291693bf81a5b4ef1551fb81cb2a5a4257081
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-07-05 15:33:47 +0300

    une programme pour savoir si un programme est null ou pas

---
+++ programs/src/main/java/com/noob/NumberSigne.java
@@ -0,0 +1,14 @@
+package com.noob;
+
+public class NumberSigne {
+  public static void main(String[] args) {
+      int number = 1;
+      if (number < 0){
+        System.out.println("Le nombre entré est Négatif.");
+      }else if(number == 0){
+        System.out.println("Le nombre entré est Zéro.");
+      }else{
+        System.out.println("Le nombre entré est Positif.");
+      }
+  }
+}
